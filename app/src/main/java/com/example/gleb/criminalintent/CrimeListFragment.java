732548aed00e4689d132d40d3f74ce2dd5b5bb5a
gleb-r <rad.gleb@gmail.com>
2017-06-07 11:22:20 +0300

    ch9 simple RecyclerView finished

--- app/src/main/java/com/example/gleb/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/gleb/criminalintent/CrimeListFragment.java
@@ -26,11 +26,15 @@ public class CrimeListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_crime_list,container,false);
         mCrimeRecyclerView = (RecyclerView)view.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        updateUI();
         return view;
     }
 
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+        mAdapter = new CrimeAdapter(crimes);
+        mCrimeRecyclerView.setAdapter(mAdapter);
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder {
